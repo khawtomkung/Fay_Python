cdb3cbcb76a5b6caeefcbc5990df56d6a87ffded
khawtomkung <napad7874@gmail.com>
2025-10-21 15:08:26 +0700

    meow

--- day-1.py
+++ day-1.py
@@ -1,1 +1,30 @@
+# comment
+# print function output the result in the terminal area
+# function_name(input_1, input_2, input_3)
+# -> argument is input of function
 print("Hello, World!")
+print("Goodbye")
+print(1, 2, 6, "\"Hello\"", sep='---') #1-2-6-"Hello"
+
+# This part of code is to
+# gether user information for login
+# ...
+# ...
+# ...
+# ...
+
+"""This is a very long
+and very helpful comment
+"""
+
+# Data Types
+"""int = 1 -> integer
+float = 1.00 -> floating point number
+Str = comment '', "" -> string
+bool = True, False -> boolean
+NOne = print(type(None)) -> NoneType
+
+abs = || ->
+round = print(round(3.14159), 2) = 3.14 
+bin = binary # เลขฐาน 2
+"""
